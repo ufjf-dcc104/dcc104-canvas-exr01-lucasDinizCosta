1f988b6adcf595e4d9c26d480a8546182205b15d
lucasDinizCosta <lucas_diniz_costa@hotmail.com>
2018-04-22 18:59:04 -0300

    Vento colocado
    O vento criado empurra a nave para a esquerda e serve para trazer uma dificuldade ao jogador

--- sprite.js
+++ sprite.js
@@ -7,74 +7,32 @@ function Sprite() {
   this.vy = 0;
   this.ax = 0;
   this.ay = 0;
-  this.ang = 0;
-  this.vang = 0;
   this.acel = 0;
   this.cor = "grey";
-  this.imunidade = 0;
   this.vento = 50;
 }
 
 Sprite.prototype.desenhar = function (ctx) {
-  /*if(this.imunidade > 0){
-    ctx.fillStyle = 'rgba(255, 255, 0, '+Math.cos(20*Math.PI*this.imunidade)+')';
-    ctx.strokeStyle = 'hsla(255,255,255, 0.3)';
-  } else {
-    ctx.fillStyle = this.cor;
-    ctx.strokeStyle = 'white';
-  }*/
   ctx.fillStyle = this.cor;
   ctx.strokeStyle = 'white';
   ctx.lineWidth = 2;
-  //ctx.save();
-  //ctx.translate(this.x, this.y);
-  //ctx.save();
-  //ctx.rotate(this.ang*Math.PI/180);
   ctx.beginPath();
   ctx.moveTo(this.x, this.y);
   ctx.lineTo(this.x + this.w/2, this.y - this.h);
   ctx.lineTo(this.x + this.w, this.y);
   ctx.lineTo(this.x, this.y);
-  //ctx.closePath();
   ctx.fill();
   ctx.stroke();
-  //ctx.restore();
-  /*if(this.debug){
-    ctx.strokeStyle = 'red';
-    ctx.lineWidth = 1;
-    ctx.strokeRect(-this.w/2, -this.h/2, this.w, this.h);
-  //ctx.fillRect(-this.w/2, -this.h/2, this.w, this.h);
-  }*/
-  //ctx.restore();
 }
 
 Sprite.prototype.mover = function (dt) {
-    //this.ang = this.ang + this.vang*dt;
 
-    this.vx = this.vx + this.ax*dt;
+    this.vx = this.vx + this.ax*dt - this.vento*dt;
     this.vy = this.vy + (G+this.ay)*dt;
 
     this.x = this.x + this.vx*dt;
     this.y = this.y + this.vy*dt;
 
- 
-    //this.imunidade = this.imunidade - 1*dt;
-}
-
-Sprite.prototype.moverAng = function (dt) {
-    this.ang = this.ang + this.vang*dt;
-
-    this.ax = this.acel*Math.cos(this.ang*Math.PI/180);
-    this.ay = this.acel*Math.sin(this.ang*Math.PI/180);
-
-    this.vx = this.vx + this.ax*dt;
-    this.vy = this.vy + (G+this.ay)*dt;
-
-    this.x = this.x + this.vx*dt;
-    this.y = this.y + this.vy*dt;
-
- 
-    this.imunidade = this.imunidade - 1*dt;
 }
 
 Sprite.prototype.perseguir = function (alvo){
