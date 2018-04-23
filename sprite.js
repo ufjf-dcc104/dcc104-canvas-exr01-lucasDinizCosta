1c04c9bb661a1c5313e52fc365bc30f3992371e7
lucasDinizCosta <lucas_diniz_costa@hotmail.com>
2018-04-22 21:15:57 -0300

    Adições:
    - Barra de energia;
    - Pontos;
    - vidas;
    - plataforma e chão;

--- sprite.js
+++ sprite.js
@@ -2,57 +2,19 @@ function Sprite() {
   this.x = 100;
   this.y = 100;
   this.w = 20;
-  this.h = 30;
-  this.vx = 0;
-  this.vy = 0;
-  this.ax = 0;
-  this.ay = 0;
-  this.acel = 0;
-  this.cor = "grey";
-  this.vento = 50;
+  this.h = 15;
+  this.colorBG = "grey";
+  this.colorBorder = "yellow";
+  this.borderSize = 1;
 }
 
 Sprite.prototype.desenhar = function (ctx) {
-  ctx.fillStyle = this.cor;
-  ctx.strokeStyle = 'white';
-  ctx.lineWidth = 2;
-  ctx.beginPath();
-  ctx.moveTo(this.x, this.y);
-  ctx.lineTo(this.x + this.w/2, this.y - this.h);
-  ctx.lineTo(this.x + this.w, this.y);
-  ctx.lineTo(this.x, this.y);
-  ctx.fill();
-  ctx.stroke();
+  ctx.fillStyle = this.colorBG;
+  ctx.strokeStyle = this.colorBorder;
+  ctx.lineWidth = this.borderSize;
+  ctx.fillRect(this.x, this.y, this.w, this.h);
+  ctx.strokeRect(this.x, this.y, this.w, this.h);
 }
-
-Sprite.prototype.mover = function (dt) {
-
-    this.vx = this.vx + this.ax*dt - this.vento*dt;
-    this.vy = this.vy + (G+this.ay)*dt;
-
-    this.x = this.x + this.vx*dt;
-    this.y = this.y + this.vy*dt;
-
-}
-
-Sprite.prototype.impoeLimites = function(x, y, w, h){
-  if(this.x < x){
-    this.x = x;
-    this.vx = 0;
-  }
-  if(this.x + this.w > x + w){
-    this.x = x + w - this.w;
-    this.vx = 0;
-  }
-  if(this.y - this.h< y){
-    this.y = 0 + this.h;
-    this.vy = 0;
-  }
-  if(this.y > y + h){
-    this.y = y + h;
-    this.vy = 0;
-  }
-};
 
 Sprite.prototype.colidiuCom = function (alvo) {
   if(alvo.x+alvo.w < this.x) return false;
